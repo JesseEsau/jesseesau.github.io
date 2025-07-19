2a25dab3e645c884a9e00427db08cc61d52d6d79
Esau Jesse <jesseesau13@gmail.com>
2025-07-19 16:43:26 +0100

    remove unused import

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,6 @@ import Contact from '@/components/Contact';
 import Footer from '@/components/Footer';
 import Header from '@/components/Header';
 import Hero from '@/components/Hero';
-import Home from '@/components/Home(mine)';
 import Projects from '@/components/Projects';
 import Skills from '@/components/Skills';
 import React from 'react'
